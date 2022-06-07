f9d47c1f89d49c6f7879227ca3b16d3d96c8a426
Kumitasu <grzybolym@gmail.com>
2022-06-07 11:25:30 +0200

    js

---
+++ app.js
@@ -0,0 +1,55 @@
+const cardArray = [
+    {
+        name: 'bird',
+        img: 'img/bird.png',
+    },
+    {
+        name: 'cat',
+        img: 'img/cat.png',
+    },
+    {
+        name: 'cow',
+        img: 'img/cow.png',
+    },
+    {
+        name: 'elephant',
+        img: 'img/elephant.png',
+    },
+    {
+        name: 'hippo',
+        img: 'img/hippo.png',
+    },
+    {
+        name: 'snake',
+        img: 'img/snake.png',
+    },
+    {
+        name: 'bird',
+        img: 'img/bird.png',
+    },
+    {
+        name: 'cat',
+        img: 'img/cat.png',
+    },
+    {
+        name: 'cow',
+        img: 'img/cow.png',
+    },
+    {
+        name: 'elephant',
+        img: 'img/elephant.png',
+    },
+    {
+        name: 'hippo',
+        img: 'img/hippo.png',
+    },
+    {
+        name: 'snake',
+        img: 'img/snake.png',
+    },
+   
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+console.log(cardArray);
